bb34d043b2722dfb86c96d7990397daa76ce684d
phirumtan <phirumtan@gmail.com>
2020-08-11 19:09:41 +0700

    for create new bindingActivity

---
+++ app/src/main/java/com/gsmarena/firstsample/DataBindingActivity.java
@@ -0,0 +1,14 @@
+package com.gsmarena.firstsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataBindingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_binding);
+    }
+}
